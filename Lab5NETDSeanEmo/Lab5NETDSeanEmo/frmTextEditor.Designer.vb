2322a9fb4210c183d25572eb7cbc3b91a94575ac
POWNxDOCTOR <emo.sean@ymail.com>
2020-04-07 15:01:07 -0400

    Add files via upload

---
+++ Lab5NETDSeanEmo/Lab5NETDSeanEmo/frmTextEditor.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTextEditor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmTextEditor))
+        Me.msMain = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UndoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RedoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TimeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.rtbText = New System.Windows.Forms.RichTextBox()
+        Me.dbOpenFile = New System.Windows.Forms.OpenFileDialog()
+        Me.dbSaveFile = New System.Windows.Forms.SaveFileDialog()
+        Me.dbFont = New System.Windows.Forms.FontDialog()
+        Me.msContext = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CopyToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CutToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectAllToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ttpTextEditor = New System.Windows.Forms.ToolTip(Me.components)
+        Me.msMain.SuspendLayout()
+        Me.msContext.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'msMain
+        '
+        Me.msMain.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.msMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.FormatToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.msMain.Location = New System.Drawing.Point(0, 0)
+        Me.msMain.Name = "msMain"
+        Me.msMain.Size = New System.Drawing.Size(1263, 24)
+        Me.msMain.TabIndex = 0
+        Me.msMain.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        Me.NewToolStripMenuItem.ToolTipText = "Opens a new text file"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        Me.OpenToolStripMenuItem.ToolTipText = "Opens up a text file from documents"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        Me.SaveToolStripMenuItem.ToolTipText = "Saves the file to a location on your computer"
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.SaveAsToolStripMenuItem.Text = "Save as"
+        Me.SaveAsToolStripMenuItem.ToolTipText = "Save file type as a certain extension"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        Me.ExitToolStripMenuItem.ToolTipText = "Exits the text editor"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UndoToolStripMenuItem, Me.RedoToolStripMenuItem, Me.CopyToolStripMenuItem, Me.CutToolStripMenuItem, Me.PasteToolStripMenuItem, Me.DeleteToolStripMenuItem, Me.SelectAllToolStripMenuItem, Me.TimeToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'UndoToolStripMenuItem
+        '
+        Me.UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        Me.UndoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Z), System.Windows.Forms.Keys)
+        Me.UndoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.UndoToolStripMenuItem.Text = "Undo "
+        Me.UndoToolStripMenuItem.ToolTipText = "Removes the last item typed by the user"
+        '
+        'RedoToolStripMenuItem
+        '
+        Me.RedoToolStripMenuItem.Name = "RedoToolStripMenuItem"
+        Me.RedoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Y), System.Windows.Forms.Keys)
+        Me.RedoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RedoToolStripMenuItem.Text = "Redo"
+        Me.RedoToolStripMenuItem.ToolTipText = "Allows you to redo something that was just done"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CopyToolStripMenuItem.Text = "Copy"
+        Me.CopyToolStripMenuItem.ToolTipText = "Copies the highlighted text in to a clipboard"
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CutToolStripMenuItem.Text = "Cut "
+        Me.CutToolStripMenuItem.ToolTipText = "Cuts the selected text in to the clipboard "
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PasteToolStripMenuItem.Text = "Paste"
+        Me.PasteToolStripMenuItem.ToolTipText = "Pastes the items stored in the clipboard "
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Delete), System.Windows.Forms.Keys)
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.DeleteToolStripMenuItem.Text = "Delete"
+        Me.DeleteToolStripMenuItem.ToolTipText = "Deletes the selected items "
+        '
+        'SelectAllToolStripMenuItem
+        '
+        Me.SelectAllToolStripMenuItem.Name = "SelectAllToolStripMenuItem"
+        Me.SelectAllToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.SelectAllToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SelectAllToolStripMenuItem.Text = "Select All"
+        Me.SelectAllToolStripMenuItem.ToolTipText = "Selects all of the items in the text editor "
+        '
+        'TimeToolStripMenuItem
+        '
+        Me.TimeToolStripMenuItem.Name = "TimeToolStripMenuItem"
+        Me.TimeToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.F5), System.Windows.Forms.Keys)
+        Me.TimeToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TimeToolStripMenuItem.Text = "Time & Date"
+        Me.TimeToolStripMenuItem.ToolTipText = "Displays the current date and time in the text editor"
+        '
+        'FormatToolStripMenuItem
+        '
+        Me.FormatToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontToolStripMenuItem})
+        Me.FormatToolStripMenuItem.Name = "FormatToolStripMenuItem"
+        Me.FormatToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.FormatToolStripMenuItem.Text = "F&ormat"
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.F), System.Windows.Forms.Keys)
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FontToolStripMenuItem.Text = "Font"
+        Me.FontToolStripMenuItem.ToolTipText = "Brings up the font dialog to select the font and size of the text."
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.B), System.Windows.Forms.Keys)
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        Me.AboutToolStripMenuItem.ToolTipText = "Displays owner of the program and information about the program"
+        '
+        'rtbText
+        '
+        Me.rtbText.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtbText.Location = New System.Drawing.Point(0, 24)
+        Me.rtbText.Name = "rtbText"
+        Me.rtbText.Size = New System.Drawing.Size(1263, 528)
+        Me.rtbText.TabIndex = 1
+        Me.rtbText.Text = ""
+        '
+        'msContext
+        '
+        Me.msContext.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem1, Me.CutToolStripMenuItem1, Me.PasteToolStripMenuItem1, Me.SelectAllToolStripMenuItem1})
+        Me.msContext.Name = "ContextMenuStrip1"
+        Me.msContext.Size = New System.Drawing.Size(123, 92)
+        '
+        'CopyToolStripMenuItem1
+        '
+        Me.CopyToolStripMenuItem1.Name = "CopyToolStripMenuItem1"
+        Me.CopyToolStripMenuItem1.Size = New System.Drawing.Size(122, 22)
+        Me.CopyToolStripMenuItem1.Text = "Copy"
+        '
+        'CutToolStripMenuItem1
+        '
+        Me.CutToolStripMenuItem1.Name = "CutToolStripMenuItem1"
+        Me.CutToolStripMenuItem1.Size = New System.Drawing.Size(122, 22)
+        Me.CutToolStripMenuItem1.Text = "Cut"
+        '
+        'PasteToolStripMenuItem1
+        '
+        Me.PasteToolStripMenuItem1.Name = "PasteToolStripMenuItem1"
+        Me.PasteToolStripMenuItem1.Size = New System.Drawing.Size(122, 22)
+        Me.PasteToolStripMenuItem1.Text = "Paste"
+        '
+        'SelectAllToolStripMenuItem1
+        '
+        Me.SelectAllToolStripMenuItem1.Name = "SelectAllToolStripMenuItem1"
+        Me.SelectAllToolStripMenuItem1.Size = New System.Drawing.Size(122, 22)
+        Me.SelectAllToolStripMenuItem1.Text = "Select All"
+        '
+        'frmTextEditor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1263, 552)
+        Me.Controls.Add(Me.rtbText)
+        Me.Controls.Add(Me.msMain)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.msMain
+        Me.Name = "frmTextEditor"
+        Me.Text = "Text Editor "
+        Me.msMain.ResumeLayout(False)
+        Me.msMain.PerformLayout()
+        Me.msContext.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents msMain As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RedoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectAllToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TimeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents rtbText As RichTextBox
+    Friend WithEvents dbOpenFile As OpenFileDialog
+    Friend WithEvents dbSaveFile As SaveFileDialog
+    Friend WithEvents dbFont As FontDialog
+    Friend WithEvents msContext As ContextMenuStrip
+    Friend WithEvents CopyToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SelectAllToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ttpTextEditor As ToolTip
+End Class
---
+++ Lab5NETDSeanEmo/Lab5NETDSeanEmo/frmTextEditor.vb
@@ -0,0 +1,173 @@
+﻿' Author: Sean Emo 
+' Date: April 7, 2020
+' Description:
+' This program is designed to act like a basic text editor like Notepad. When executed 
+' the user can enter text in to the textbox and can then save that text to the file 
+' destination that they choose. I was very stressed out about this lab and I didn't really 
+' know too much of what I was doing so I had to look up a bunch of resources online. I 
+' feel like I did the best considering this new learning system. I really only learn by doing
+' so being able to follow along with tutorials or repetition in typing is the way I learn. 
+
+' Added System.IO to allow for reading and writing files, basic file support that a text editor would need. 
+Imports System.IO
+
+Public Class frmTextEditor
+    '' Variable declaration
+    Dim ind As Byte = 0
+    Dim path As String
+    ' Opens up the form to the center of the screen when the form loads. 
+    Private Sub frmTextEditor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+#Region "ToolStrip Items"
+
+    ' Below allows for the user to click new and a new form appears as a dialog, so it opens up a new
+    ' instance of the program. 
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Dim newForm As New frmTextEditor
+        newForm.ShowDialog()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Dim myStream As Stream = Nothing
+        ' The filter below opens up in text files for the path because it was required by the program, if we 
+        ' change the values to bmp, jpg and replace the variables with the text type you can change the default save 
+        ' file type. 
+        dbOpenFile.Filter = "Text Files (*.txt)|*.txt|All Files(*.*)|*.*"
+        dbOpenFile.FilterIndex = 1
+        dbOpenFile.RestoreDirectory = True
+        ' The file is attempting to open the openfile dialog box and checking to make sure the file itself is not null. 
+        If (dbOpenFile.ShowDialog() = System.Windows.Forms.DialogResult.OK) And (dbOpenFile.FileName.Length > 0) Then
+            Try
+                ' If that works then the stream opens the file according to the path of the filename and loads the file. 
+                myStream = dbOpenFile.OpenFile
+                If (myStream IsNot Nothing) Then
+                    rtbText.LoadFile(dbOpenFile.FileName, RichTextBoxStreamType.PlainText)
+                    path = dbOpenFile.FileName
+                End If
+                ' If the file cannot be loaded an error message occurs. 
+            Catch ex As Exception
+                MessageBox.Show("Cannot read file from disk, Original error." & ex.Message)
+            Finally
+                If (myStream IsNot Nothing) Then
+                    ind = 1
+                    myStream.Close()
+                End If
+            End Try
+        End If
+
+    End Sub
+    ' Save file dialog below. 
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        ' Repetition of code, I'm glad I could rewrite these a few times to learn how to use them and what they kind of do. 
+        ' So a line called save text files appears, it checks to see if the path or file exists and then allows for the saving 
+        ' of default txt files, again we could change that by changing the filter line with a different extension type. 
+        dbSaveFile.Title = "Save Text Files"
+        dbSaveFile.CheckFileExists = True
+        dbSaveFile.CheckPathExists = True
+        dbSaveFile.DefaultExt = "txt"
+        dbSaveFile.Filter = "Text Files (*.txt)|*.txt|All Files(*.*)|*.*"
+        dbSaveFile.FilterIndex = 1
+        dbSaveFile.RestoreDirectory = True
+
+        ' If the file or path exists it tries to save the textbox to the save file. 
+        Try
+            rtbText.SaveFile(dbSaveFile.FileName, RichTextBoxStreamType.PlainText)
+
+        Catch ex As Exception
+            Call SaveAsToolStripMenuItem_Click(Me, e)
+        End Try
+    End Sub
+    ' This button does pretty much the same thing as above, hence the repititon which is helping me learn. 
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        ' This file is checking the opposite of the save button above, since you are saving as, you are automatically 
+        ' showing the dialog box to choose where and how you want to save, there is no need to check if the file or path
+        ' exists. It then brings up the save file dialog box and allows the user to choose where to save the txt file. 
+        dbSaveFile.Title = "Save Text Files"
+        dbSaveFile.CheckFileExists = False
+        dbSaveFile.CheckPathExists = False
+        dbSaveFile.DefaultExt = "txt"
+        dbSaveFile.Filter = "Text Files (*.txt)|*.txt|All Files(*.*)|*.*"
+        dbSaveFile.FilterIndex = 1
+        dbSaveFile.RestoreDirectory = True
+
+        If (dbSaveFile.ShowDialog() = DialogResult.OK) Then
+            rtbText.SaveFile(dbSaveFile.FileName, RichTextBoxStreamType.PlainText)
+        End If
+    End Sub
+    ' Closes the application when the exit button is hit. 
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+    ' Undoes the last thing typed by the user, all of these built in functions helped quite a bit. 
+    Private Sub UndoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UndoToolStripMenuItem.Click
+        rtbText.Undo()
+    End Sub
+    ' If a user undid something and made a mistake doing that then the redo button allows them to undo a mistake they undid. 
+    Private Sub RedoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedoToolStripMenuItem.Click
+        rtbText.Redo()
+    End Sub
+    ' Copies the text in the textbox as long as the textbox isn't empty. 
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        If rtbText.SelectionLength > 0 Then
+            rtbText.Copy()
+        End If
+    End Sub
+    ' Cuts the contents of the textbox to the clipboard as long as there is something in the textbox, like the copy button. 
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        If rtbText.SelectionLength > 0 Then
+            rtbText.Cut()
+        End If
+    End Sub
+    ' pastes whatever has been copied to the clipboard for the user. 
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        rtbText.Paste()
+    End Sub
+    ' Sets the textbox to empty if the user hits the delete button. 
+    Private Sub DeleteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteToolStripMenuItem.Click
+        Me.rtbText.SelectedText = ""
+    End Sub
+    ' The select all button highlights all text inside the textbox to be used to cut or copy. 
+    Private Sub SelectAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectAllToolStripMenuItem.Click
+        rtbText.SelectAll()
+    End Sub
+    ' This one is a little interesting, I looked this one up to add as well, it is a timestamp that inserts the current date and time 
+    ' in to the program whenever the user clicks it, it goes to the internal system clock and gets the current date and time as of when 
+    ' it was clicked then displays that in the current format below dd/mm/yyyy hh:mm:ss. 
+    Private Sub TimeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TimeToolStripMenuItem.Click
+        rtbText.SelectedText = System.DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")
+    End Sub
+    ' This button I saw as well and decided to add it for more functionality, by adding the font dialog box when the user clicks the font 
+    ' button, allowing them to change their font, it then sets the font to the textbox. 
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        dbFont.Font = rtbText.Font
+        dbFont.ShowDialog()
+        rtbText.Font = dbFont.Font
+    End Sub
+    ' Same as the copy button above, but for the contextual menu
+    Private Sub CopyToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem1.Click
+        If rtbText.SelectionLength > 0 Then
+            rtbText.Copy()
+        End If
+    End Sub
+    ' Same as the cut procedure above, just adding them in for the contextual menu. 
+    Private Sub CutToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem1.Click
+        If rtbText.SelectionLength > 0 Then
+            rtbText.Cut()
+        End If
+    End Sub
+    ' Pastes the text that has been copied to the clipboard, same as above. 
+    Private Sub PasteToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem1.Click
+        rtbText.Paste()
+    End Sub
+    ' Selects all text and highlights it, same as above. 
+    Private Sub SelectAllToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SelectAllToolStripMenuItem1.Click
+        rtbText.SelectAll()
+    End Sub
+    ' I saw the help button had an about the user, so I added a message box that pops up with my name, assignment, and course in the 
+    ' description. 
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MsgBox("NETD 2202" & vbCrLf & "Lab 5" & vbCrLf & "Sean Emo" & vbCrLf)
+    End Sub
+End Class
+#End Region
